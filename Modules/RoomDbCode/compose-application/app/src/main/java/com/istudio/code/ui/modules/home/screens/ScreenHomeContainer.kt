df2f7b9d7b842a5bc7833b1b0eb536f511f8e95e
devrath dev <devrath.dev595@gmail.com>
2023-10-15 22:58:22 +0530

    more code added

--- Modules/RoomDbCode/compose-application/app/src/main/java/com/istudio/code/ui/modules/home/screens/ScreenHomeContainer.kt
+++ Modules/RoomDbCode/compose-application/app/src/main/java/com/istudio/code/ui/modules/home/screens/ScreenHomeContainer.kt
@@ -1,47 +1,58 @@
 package com.istudio.code.ui.modules.home.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.istudio.code.R
 import com.istudio.code.ui.modules.home.navigation.HomeModuleNavGraph
 import com.istudio.code.ui.modules.home.navigation.HomeNavItem
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeContainer(modifier: Modifier = Modifier) {
 
+    // Context
     val cxt = LocalContext.current
-
+    // Coroutine to handle the animation
+    val coroutineScope = rememberCoroutineScope()
     // Data items to have the Bottom tabs
     val navItems = remember {
-        mutableStateListOf(
-            HomeNavItem.MyBooks,
-            HomeNavItem.BookReviews,
-            HomeNavItem.ReadingList
-        )
+        mutableStateListOf(HomeNavItem.MyBooks, HomeNavItem.BookReviews, HomeNavItem.ReadingList)
     }
 
     // Scroll behaviour
@@ -50,74 +61,112 @@ fun HomeContainer(modifier: Modifier = Modifier) {
     var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
     // Keeping track of navigation
     val navController = rememberNavController();
+    // For -> BottomSheetScaffold
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
 
-
-    // Scaffold: it contains bottom navigation bar and a NavHost(Contains all screens) for each screen
-    Scaffold(
-        modifier = Modifier
-            .fillMaxSize()
-            .nestedScroll(scrollBehaviour.nestedScrollConnection),
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    var screenTitle = cxt.getString(R.string.str_home)
-                    screenTitle = when (selectedItemIndex) {
-                        0 -> cxt.getString(R.string.str_my_books)
-                        1 -> cxt.getString(R.string.str_book_reviews)
-                        else -> cxt.getString(R.string.str_reading_list)
-                    }
-                    Text(text = screenTitle)
-                },
-                actions = {
-                    if(selectedItemIndex == 0){
-                        // Display the filter Icon only if it is First Tab - MyBooks
-                        IconButton(onClick = {
-
-                        }) {
-                            Icon(
-                                imageVector = Icons.Default.FilterList,
-                                contentDescription = cxt.getString(R.string.str_filter)
-                            )
-                        }
-                    }
-                },
-                scrollBehavior = scrollBehaviour
-            )
+    BottomSheetScaffold(
+        scaffoldState = bottomSheetScaffoldState,
+        sheetContent = {
+            BottomSheetContent()
         },
-        bottomBar = {
-            NavigationBar {
-                navItems.forEachIndexed { index, item ->
-                    NavigationBarItem(
-                        // If both the index are same, return true indicating it is selected
-                        selected = (selectedItemIndex == index),
-                        onClick = {
-                            selectedItemIndex = index
-                            navController.navigate(item.route)
-                        },
-                        label = {
-                            Text(text = cxt.getString(item.title))
-                        },
-                        icon = {
-                            Icon(
-                                imageVector = if (selectedItemIndex == index) {
-                                    item.iconSelected
-                                } else {
-                                    item.iconUnSelected
-                                },
-                                contentDescription = cxt.getString(item.title)
-                            )
-                        },
-                    )
-                }
-            }
-        }
+        sheetPeekHeight = 0.dp
     ) {
-        Column(
+
+        // Scaffold: it contains bottom navigation bar and a NavHost(Contains all screens) for each screen
+        Scaffold(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it)
+                .nestedScroll(scrollBehaviour.nestedScrollConnection),
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        var screenTitle = cxt.getString(R.string.str_home)
+                        screenTitle = when (selectedItemIndex) {
+                            0 -> cxt.getString(R.string.str_my_books)
+                            1 -> cxt.getString(R.string.str_book_reviews)
+                            else -> cxt.getString(R.string.str_reading_list)
+                        }
+                        Text(text = screenTitle)
+                    },
+                    actions = {
+                        if(selectedItemIndex == 0){
+                            // Display the filter Icon only if it is First Tab - MyBooks
+                            IconButton(onClick = {
+                                coroutineScope.launch {
+                                    bottomSheetScaffoldState.bottomSheetState.expand()
+                                }
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Default.FilterList,
+                                    contentDescription = cxt.getString(R.string.str_filter)
+                                )
+                            }
+                        }
+                    },
+                    scrollBehavior = scrollBehaviour
+                )
+            },
+            bottomBar = {
+                NavigationBar {
+                    navItems.forEachIndexed { index, item ->
+                        NavigationBarItem(
+                            // If both the index are same, return true indicating it is selected
+                            selected = (selectedItemIndex == index),
+                            onClick = {
+                                selectedItemIndex = index
+                                navController.navigate(item.route)
+                            },
+                            label = {
+                                Text(text = cxt.getString(item.title))
+                            },
+                            icon = {
+                                Icon(
+                                    imageVector = if (selectedItemIndex == index) {
+                                        item.iconSelected
+                                    } else {
+                                        item.iconUnSelected
+                                    },
+                                    contentDescription = cxt.getString(item.title)
+                                )
+                            },
+                        )
+                    }
+                }
+            }
         ) {
-            HomeModuleNavGraph(navController)
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it)
+            ) {
+                HomeModuleNavGraph(navController)
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun BottomSheetContent() {
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(4) { currentNumber ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(vertical = 25.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    "Current Row -> $currentNumber",
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
         }
     }
 }
