113687ef9aaa18f6a968b8e42f356b4dc61c82d2
devrath dev <devrath.dev595@gmail.com>
2023-10-15 19:14:50 +0530

    Added top App bar

--- Modules/RoomDbCode/compose-application/app/src/main/java/com/istudio/code/ui/modules/home/screens/ScreenHomeContainer.kt
+++ Modules/RoomDbCode/compose-application/app/src/main/java/com/istudio/code/ui/modules/home/screens/ScreenHomeContainer.kt
@@ -3,24 +3,36 @@ package com.istudio.code.ui.modules.home.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.navigation.compose.rememberNavController
 import com.istudio.code.ui.modules.home.navigation.HomeModuleNavGraph
 import com.istudio.code.ui.modules.home.navigation.HomeNavItem
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeContainer(modifier: Modifier = Modifier) {
 
@@ -33,12 +45,37 @@ fun HomeContainer(modifier: Modifier = Modifier) {
         )
     }
 
+    // Scroll behaviour
+    val scrollBehaviour = TopAppBarDefaults.pinnedScrollBehavior()
     // The state "rememberSavable" helps to save the state in configuration changes, We can save in viewmodel also
     var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
     // Keeping track of navigation
     val navController = rememberNavController();
+
+
     // Scaffold: it contains bottom navigation bar and a NavHost(Contains all screens) for each screen
     Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehaviour.nestedScrollConnection),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Home")
+                },
+                actions = {
+                    IconButton(onClick = {
+
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.FilterList,
+                            contentDescription = "Filter"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehaviour
+            )
+        },
         bottomBar = {
             NavigationBar {
                 navItems.forEachIndexed { index, item ->
