8e0c33998b3045f711adf7cfce3b86a54f7c0736
devrath dev <devrath.dev595@gmail.com>
2023-10-17 12:12:56 +0530

    RoomDb dependencies added

--- Modules/RoomDbCode/compose-application/app/build.gradle.kts
+++ Modules/RoomDbCode/compose-application/app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    kotlin("kapt")
+    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
     id("com.google.dagger.hilt.android")
 }
 
@@ -59,7 +61,6 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -92,4 +93,14 @@ dependencies {
     // Determine whether you are on phone, tablet etc to determine the right UI layout
     implementation("androidx.compose.material3:material3-window-size-class:1.2.0-alpha09")
     // <---------------------- MATERIAL-3 ---------------------->
+    // <------------------- Coroutines -------------------------->
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    // <------------------- Coroutines -------------------------->
+    // <------------------- Room Database ---------------------->
+    implementation("androidx.room:room-runtime:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+    // <------------------- Room Database ---------------------->
+
 }
