55c1eeb15a2d1bddc6d775857c9f3ec22a3deb85
Devrath <devrath.dev595@gmail.com>
2023-10-22 23:10:32 +0530

    Removed xml code

--- Modules/RoomDbCode/xml-application/app/src/main/java/com/example/code/model/BookItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.code.model
-
-data class BookItem(
-    val bookId: String,
-    val name: String,
-    var isSelected: Boolean = false
-)
--- Modules/RoomDbCode/xml-application/app/src/main/java/com/example/code/utils/DateUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.example.code.utils
-
-import java.text.SimpleDateFormat
-import java.util.*
-
-private val simpleDateFormat = SimpleDateFormat.getDateTimeInstance()
-
-fun formatDateToText(date: Date): String = simpleDateFormat.format(date)
